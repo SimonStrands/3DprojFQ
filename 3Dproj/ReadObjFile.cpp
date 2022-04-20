870142206e4ce1e74ebed87d17d82acb2d68b32b
Simon Strand <etta11@hotmail.se>
2022-04-20 10:38:56 +0200

    loads faster now

--- 3Dproj/ReadObjFile.cpp
+++ 3Dproj/ReadObjFile.cpp
@@ -182,7 +182,6 @@ void createMesh(Graphics*& gfx, std::vector<MeshObj> &Meshes, std::vector<vertex
 	fixtangent(vertecies);
 	MeshObj a(gfx, vertecies, matrial);
 	Meshes.push_back(a);
-	//Meshes.push_back(MeshObj(gfx, vertecies, matrial));
 	if (matrial->flags.Maps[3]) {
 		Meshes[Meshes.size() - 1].SetShaders(gfx->getVS()[0], gfx->getPS()[0]);
 	}
@@ -200,32 +199,34 @@ void readFace(std::string readWord, std::vector<vertex> &vertecies, std::vector<
 	std::string* sTemp;
 	std::string sTemp2[4];
 	std::string trash;
-		std::istringstream a;
+	char trashChar;
+		std::stringstream a;
 		a.str(readWord);
 		a >> trash >> sTemp2[0] >> sTemp2[1] >> sTemp2[2] >> sTemp2[3];
+		std::stringstream b;
+		int indeciesVertecies, indeciesUV, indeciesNormals;
+		
 		if (sTemp2[3] != "") {
+			
 			for (int i = 0; i < 3; i++) {
-				//nrOfVertexes++;
-				sTemp = getDest(sTemp2[i]);
-				//när jag läser in faces så får dem första sex alltid samma p.g.a det är så dem har skrivit det på obj filen
-				vertecies.push_back(vertex(vPos[std::stoi(sTemp[0]) - 1], vUv[std::stoi(sTemp[1]) - 1], vNorm[std::stoi(sTemp[2]) - 1]));
-				delete[] sTemp;
+				b.str(sTemp2[i]);
+				b >> indeciesVertecies >> trashChar >> indeciesUV >> trashChar >> indeciesNormals;
+				vertecies.push_back(vertex(vPos[indeciesVertecies - 1], vUv[indeciesUV - 1], vNorm[indeciesNormals - 1]));
+				b.clear();
 			}
-			sTemp = getDest(sTemp2[0]);
-			vertecies.push_back(vertex(vPos[std::stoi(sTemp[0]) - 1], vUv[std::stoi(sTemp[1]) - 1], vNorm[std::stoi(sTemp[2]) - 1]));
-			delete[] sTemp;
-			sTemp = getDest(sTemp2[2]);
-			vertecies.push_back(vertex(vPos[std::stoi(sTemp[0]) - 1], vUv[std::stoi(sTemp[1]) - 1], vNorm[std::stoi(sTemp[2]) - 1]));
-			delete[] sTemp;
-			sTemp = getDest(sTemp2[3]);
-			vertecies.push_back(vertex(vPos[std::stoi(sTemp[0]) - 1], vUv[std::stoi(sTemp[1]) - 1], vNorm[std::stoi(sTemp[2]) - 1]));
-			delete[] sTemp;
+			b.str(sTemp2[3]);
+			b >> indeciesVertecies >> trashChar >> indeciesUV >> trashChar >> indeciesNormals;
+			vertecies.push_back(vertecies[vertecies.size() - 1]);
+			vertecies.push_back(vertex(vPos[indeciesVertecies - 1], vUv[indeciesUV - 1], vNorm[indeciesNormals - 1]));
+			vertecies.push_back(vertecies[vertecies.size() - 5]);
+			b.clear();
 		}
 		else {
 			for (int i = 0; i < 3; i++) {
-				sTemp = getDest(sTemp2[i]);
-				vertecies.push_back(vertex(vPos[std::stoi(sTemp[0]) - 1], vUv[std::stoi(sTemp[1]) - 1], vNorm[std::stoi(sTemp[2]) - 1]));
-				delete[] sTemp;
+				b.str(sTemp2[i]);
+				b >> indeciesVertecies >> trashChar >> indeciesUV >> trashChar >> indeciesNormals;
+				vertecies.push_back(vertex(vPos[indeciesVertecies - 1], vUv[indeciesUV - 1], vNorm[indeciesNormals - 1]));
+				b.clear();
 			}
 		}
 }
@@ -326,133 +327,3 @@ bool readObjFile(std::vector<MeshObj>& Meshes, std::string fileName, std::vector
 	}
 	return true;
 }
-/*
-bool readObjFile2(std::vector<MeshObj>& Meshes, std::string fileName, std::vector<Material> matrial, Graphics *& gfx)
-{
-	std::string* sTemp;
-	std::string sTemp2[4];
-
-	std::vector<vertex> vertecies;
-	std::vector<std::array<float, 3>>vPos;
-	std::vector<std::array<float, 2>>vUv;
-	std::vector<std::array<float, 4>>vNorm;
-	int currentMatrial = -1;
-
-	std::ifstream infile(fileName);
-	std::string readWord;
-	std::string trash;
-	bool first = true;
-	if (!infile.is_open()) {
-
-		return false;
-	}
-
-	while (std::getline(infile, readWord)) {
-		//get all data
-		if (readWord.substr(0, 2) == "v ") {
-			std::istringstream a;
-			vPos.resize(vPos.size() + 1);
-			a.str(readWord);
-			a >> trash >> vPos[vPos.size() - 1][0] >> vPos[vPos.size() - 1][1] >> vPos[vPos.size() - 1][2];
-		}
-		else if (readWord.substr(0, 3) == "vt ") {
-			std::istringstream a;
-			a.str(readWord);
-			vUv.resize(vUv.size() + 1);
-			a >> trash >> vUv[vUv.size() - 1][0] >> vUv[vUv.size() - 1][1];
-		}
-		else if (readWord.substr(0, 3) == "vn ") {
-			std::istringstream a;
-			a.str(readWord);
-			vNorm.resize(vNorm.size() + 1);
-			a >> trash >> vNorm[vNorm.size() - 1][0] >> vNorm[vNorm.size() - 1][1] >> vNorm[vNorm.size() - 1][2];
-			vNorm[vNorm.size() - 1][3] = 0;
-		}
-		//make data to vertex
-		//ask if this is ok
-		else if (readWord.substr(0, 2) == "f ") {
-			std::istringstream a;
-			a.str(readWord);
-			a >> trash >> sTemp2[0] >> sTemp2[1] >> sTemp2[2] >> sTemp2[3];
-			if (sTemp2[3] != "") {
-				for (int i = 0; i < 3; i++) {
-					//nrOfVertexes++;
-					sTemp = getDest(sTemp2[i]);
-					//när jag läser in faces så får dem första sex alltid samma p.g.a det är så dem har skrivit det på obj filen
-					vertecies.push_back(vertex(vPos[std::stoi(sTemp[0]) - 1], vUv[std::stoi(sTemp[1]) - 1], vNorm[std::stoi(sTemp[2]) - 1]));
-					delete[] sTemp;
-				}
-				//nrOfVertexes += 3;
-				sTemp = getDest(sTemp2[0]);
-				vertecies.push_back(vertex(vPos[std::stoi(sTemp[0]) - 1], vUv[std::stoi(sTemp[1]) - 1], vNorm[std::stoi(sTemp[2]) - 1]));
-				delete[] sTemp;
-				sTemp = getDest(sTemp2[2]);
-				vertecies.push_back(vertex(vPos[std::stoi(sTemp[0]) - 1], vUv[std::stoi(sTemp[1]) - 1], vNorm[std::stoi(sTemp[2]) - 1]));
-				delete[] sTemp;
-				sTemp = getDest(sTemp2[3]);
-				vertecies.push_back(vertex(vPos[std::stoi(sTemp[0]) - 1], vUv[std::stoi(sTemp[1]) - 1], vNorm[std::stoi(sTemp[2]) - 1]));
-				delete[] sTemp;
-			}
-			else {
-				for (int i = 0; i < 3; i++) {
-					//nrOfVertexes++;
-					sTemp = getDest(sTemp2[i]);
-					vertecies.push_back(vertex(vPos[std::stoi(sTemp[0]) - 1], vUv[std::stoi(sTemp[1]) - 1], vNorm[std::stoi(sTemp[2]) - 1]));
-					delete[] sTemp;
-				}
-			}
-		}
-		else if (readWord.substr(0, 6) == "mtllib")
-		{
-			std::istringstream a;
-			a.str(readWord);
-			std::string mtlname;
-			a >> trash >> mtlname;
-		}
-		else if (readWord.substr(0, 2) == "o ") {
-			if (!first) {
-				fixtangent(vertecies);
-				Meshes.push_back(MeshObj(gfx, vertecies, matrial[currentMatrial]));
-				if (matrial[currentMatrial].flags.Maps[3]) {
-					Meshes[Meshes.size() - 1].SetShaders(gfx->getVS()[0], gfx->getPS()[0]);
-				}
-				else {
-					Meshes[Meshes.size() - 1].SetShaders(gfx->getVS()[0], gfx->getPS()[2]);
-				}
-				if (matrial[currentMatrial].flags.Maps[4]) {
-					Meshes[Meshes.size() - 1].SetShaders(gfx->getVS()[2]);
-					Meshes[Meshes.size() - 1].SetShaders(gfx->getHS()[0], gfx->getDS()[0]);
-				}
-				vertecies.clear();
-			}
-			first = false;
-		}
-		else if (readWord.substr(0, 6) == "usemtl") {
-			std::istringstream a;
-			a.str(readWord);
-			std::string mName;
-			a >> trash >> mName;
-			for (int i = 0; i < matrial.size(); i++) {
-				if (matrial[i].name == mName) {
-					currentMatrial = i;
-				}
-			}
-		}
-	}
-	fixtangent(vertecies);
-	Meshes.push_back(MeshObj(gfx, vertecies, matrial[currentMatrial]));
-	if (matrial[currentMatrial].flags.Maps[3]) {
-		Meshes[Meshes.size() - 1].SetShaders(gfx->getVS()[0], gfx->getPS()[0]);
-	}
-	else {
-		Meshes[Meshes.size() - 1].SetShaders(gfx->getVS()[0], gfx->getPS()[2]);
-	}
-	if (matrial[currentMatrial].flags.Maps[4]) {
-		Meshes[Meshes.size() - 1].SetShaders(gfx->getVS()[2]);
-		Meshes[Meshes.size() - 1].SetShaders(gfx->getHS()[0], gfx->getDS()[0]);
-		//Meshes[Meshes.size() - 1].SetShaders(gfx->getHS()[1], gfx->getDS()[1]);
-	}
-	
-	return true;
-}
-*/
